a077980ebe438bc8fc5416ce55ae18df63042c4f
leonskidev <leonskidev@pm.me>
2021-09-30 07:00:20 +0100

    test: let's see where the problem is

--- cors.ts
+++ cors.ts
@@ -1,6 +1,7 @@
 // this is a cors proxy for the replit gql endpoint which is hosted on
 // https://deno.com/deploy. no, this will only work for my specific endpoint,
-// no yours. i'm trying to avoid ddosing deno deploy's servers
+// no yours. i'm trying to avoid ddosing deno deploy's servers. feel free
+// to copy this code and host it yourself, however
 
 import { serve } from "https://deno.land/x/sift@0.3.5/mod.ts";
 
@@ -9,7 +10,7 @@ const HEADERS = new Headers({
   "Content-Type": "application/json",
   "Accept": "application/json",
   "Connection": "keep-alive",
-  "X-Requested-With": "the ReplAPI.it Project",
+  "X-Requested-With": "the whois project",
   "Referrer": "https://replit.com/",
   "Origin": "https://replit.com/",
 });
@@ -22,14 +23,20 @@ serve({
     //   return new Response(null, { status: 401 });
     // }
 
+    let json = await req.text();
+    console.log("REQ:", json);
+
     const api = await fetch(
       API_URL,
       {
         method: "POST",
         headers: HEADERS,
-        body: await req.text(),
+        body: json,
       }
     );
+
+    json = await api.text();
+    console.log("RES:", json); 
 
     // api.headers.set("Access-Control-Allow-Origin", "https://whois.repl.co");
     // api.headers.set("Vary", "Origin");
